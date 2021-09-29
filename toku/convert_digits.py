73e966b61a5747107875f793a9c8aa930d99f2a9
ihojmanb <ihojmanb@gmail.com>
2021-09-29 16:26:56 -0300

    setup script and test

---
+++ toku/convert_digits.py
@@ -0,0 +1,13 @@
+# a function receives a string 'input_string' and two integers,
+# 'start_position' and 'end_position'. The function needs to 
+# convert the numbers to its equivalent alphabetical capitalized form
+# and return the input_string with the number digits modified between 
+# the start_position and end_position
+
+# if end_position > length of input_string, return "INVALID"
+# if start_position > end_position, return "INVALID"
+# if start_position < 1, return "INVALID"
+# if start_position is 1, it means to start at the first character of the
+# input_string
+
+
---
+++ toku/convert_digits_test.py
@@ -0,0 +1,8 @@
+import unittest
+from convert_digits import *
+class ConvertDigitsTest(unittest.TestCase):
+    def test_digit_convertion(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
