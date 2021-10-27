bb00b4ea7c2a9c132d5980243a1919051730c2f3
ihojmanb <ihojmanb@gmail.com>
2021-10-27 01:49:36 -0300

    add annualized return method to Portfolio class

--- fintual/src/stocks/stocks.py
+++ fintual/src/stocks/stocks.py
@@ -1,5 +1,7 @@
 import json
 from functools import reduce
+from datetime import datetime
+import math
 
 # Setup
 stocks_database = open("fake_data.json")
@@ -29,26 +31,29 @@ class Stock:
         self._amount = amount
 
     def price(self, date: str):
-            stock_price = self.get_stock_price_from_database(date)
-            return stock_price
-
+        stock_price = self.get_stock_price_from_database(date)
+        return stock_price
 
     def get_stock_price_from_database(self, date):
         if self.symbol in available_tickers:
-            stock_data = [
-                stock for stock in stocks_database
-                if stock["ticker"] == self.symbol
-            ]
-
+            stock_data = self.get_stock_from_database()
             stock_price = stock_data[0]["prices"][date]
             return stock_price
         else:
             raise KeyError("ticker is not in our database.")
 
+    def get_stock_from_database(self):
+        stock_data = [
+            stock for stock in stocks_database
+            if stock["ticker"] == self.symbol
+        ]
+        return stock_data
+
 
 class Portfolio:
     def __init__(self, stock_collection: dict) -> None:
         self._stocks = self.set_stock_collection(stock_collection)
+        self.utils = PortfolioUtils()
 
     @property
     def stocks(self):
@@ -81,7 +86,7 @@ class Portfolio:
     def _calculate_profit(self, stocks_buy_prices, stocks_sell_prices):
         list_of_profit_by_stock = self.substract_buy_to_sell_prices(
             stocks_buy_prices, stocks_sell_prices)
-        profit = reduce(lambda x, y: x + y, list_of_profit_by_stock)
+        profit = self.sum_stock_profits(list_of_profit_by_stock)
         return profit
 
     def get_stocks_amount(self):
@@ -102,11 +107,130 @@ class Portfolio:
         return [a-b for a, b in zip(list_of_total_stocks_sell_prices,
                                     list_of_total_stocks_buy_prices)]
 
+    def sum_stock_profits(self, list_of_profit_by_stock):
+        profit = reduce(lambda x, y: x + y, list_of_profit_by_stock)
+        return profit
+
+    def annualized_return(self, initial_date, ending_date, measure="year"):
+
+        initial_investment = self.sum_stock_profits(
+            self.get_total_stocks_prices(initial_date))
+
+        profit = self.profit(initial_date, ending_date)
+        # R
+        investment_return = profit/initial_investment
+        # t
+        time_between_dates = self.utils.get_time_between(
+            initial_date, ending_date, measure=measure, format="%Y-%m-%d")
+        # r
+        annualized_return = math.pow(
+            (1 + investment_return), 1/time_between_dates) - 1
+        return annualized_return
+
+
+# Utility class for helper method that are out of the business logic of
+# Portfolio Class
+# TODO refactor this class, altough not a core component.
+class PortfolioUtils:
+    def __init__(self):
+        pass
+
+    def get_time_between(self, initial_date, ending_date, measure: str, format: str):
+        days_between_dates = self.get_days_between(
+            initial_date, ending_date, format)
+
+        if measure == "day":
+            return days_between_dates
+
+        elif measure == "month":
+            months_between_dates = round((days_between_dates / 365) * 12, 3)
+            # months_between_dates = self.get_rounded("month", days_between_dates)
+            return months_between_dates
+
+        elif measure == "year":
+            years_between_dates = round((days_between_dates / 365), 3)
+            # years_between_dates = self.get_rounded("year", days_between_dates)
+            return years_between_dates
+
+    def get_rounded(self, measure, time):
+        if measure == "month":
+            return self.get_rounded_month(time)
+        elif measure == "year":
+            return self.get_rounded_year(time)
+  
+    def get_rounded_month(self, days_between_dates):
+        amount= (days_between_dates / 365) * 12
+        return self.non_zero_round_calculator(amount)
+
+    def get_rounded_year(self, days_between_dates):
+        amount= days_between_dates / 365
+        return self.non_zero_round_calculator(amount)
+
+    def non_zero_round_calculator(self, amount):
+        total_digits = 2
+        rounded_days_between_dates = round(amount, total_digits)
+        while rounded_days_between_dates == 0:
+            total_digits += 1
+            rounded_days_between_dates = round(amount, total_digits)
+        return rounded_days_between_dates  
+
+
+
+    def get_days_between(self, initial_date, ending_date, format: str):
+        initial_datetime_object = self.transform_string_date_to_datetime(
+            initial_date, format)
+        ending_datetime_object = self.transform_string_date_to_datetime(
+            ending_date, format)
+
+        delta_time_object = abs(
+            ending_datetime_object - initial_datetime_object)
+
+        days_between_dates = delta_time_object.days
+
+        return days_between_dates
+
+    def transform_string_date_to_datetime(self, date, format):
+        return datetime.strptime(date, format)
+
+    def report_annualized_return(self, initial_date, ending_date, annualized_return):
+        format = "%Y-%m-%d"
+        days_between_dates = self.get_days_between(
+            initial_date, ending_date, format)
+        if days_between_dates <= 31:
+            return self.report(annualized_return, measure="day")
+
+        elif days_between_dates > 31 and days_between_dates < 365:
+            return self.report(annualized_return, measure="month")
+
+        elif days_between_dates >= 365:
+            return self.report(annualized_return, measure="year")
+
+    def report(self, annualized_return, measure: str):
+        report = f"""annualized return is {round(annualized_return*100, 2)}% per {measure}
+         with reinvestment."""
+        return report
+
+
 portfolio = Portfolio({
-    "AAPL": 100,
-    "INNT": 50,
+    "AAPL": 1,
+    "INNT": 1,
     "GOOGL": 2,
-    "FB": 300
+    "FB": 3
 })
-portfolio_profit = portfolio.profit("2020-02-11", "2020-11-03")
-print(portfolio_profit)
+
+
+if __name__ == "__main__":
+    annualized_return = portfolio.annualized_return("2019-01-01", "2019-01-02")
+    report = portfolio.utils.report_annualized_return(
+        "2019-01-01", "2019-01-02", annualized_return)
+    print(report)
+
+    annualized_return = portfolio.annualized_return("2019-01-01", "2019-02-02")
+    report = portfolio.utils.report_annualized_return(
+        "2019-01-01", "2019-02-02", annualized_return)
+    print(report)
+
+    annualized_return = portfolio.annualized_return("2019-01-01", "2020-12-31")
+    report = portfolio.utils.report_annualized_return(
+        "2019-01-01", "2020-12-31", annualized_return)
+    print(report)
--- fintual/tests/test_portfolio_class/test_portfolio_class.py
+++ fintual/tests/test_portfolio_class/test_portfolio_class.py
@@ -1,8 +1,10 @@
 from stocks.stocks import Portfolio
 import pytest
+import math
+
 
 class TestPortfolioClass:
-    
+
     def test_portfolio_stock_collection_is_not_empty(self):
         portfolio = Portfolio({
             "AAPL": 100,
@@ -17,7 +19,6 @@ class TestPortfolioClass:
         portfolio = Portfolio({})
         assert len(portfolio.stocks) == 0
 
-
     def test_portfolio_profit_method_success(self):
         portfolio = Portfolio({
             "AAPL": 100,
@@ -28,10 +29,10 @@ class TestPortfolioClass:
         portfolio_profit = portfolio.profit("2019-01-01", "2020-12-31")
         theoretical_profit = 0
         for stock in portfolio.stocks:
-            profit = stock.amount * (stock.price("2020-12-31")- stock.price("2019-01-01"))
+            profit = stock.amount * \
+                (stock.price("2020-12-31") - stock.price("2019-01-01"))
             theoretical_profit += profit
         assert portfolio_profit == theoretical_profit
-
 
     def test_portfolio_profit_method_success_fail(self):
         portfolio = Portfolio({
@@ -43,13 +44,94 @@ class TestPortfolioClass:
         with pytest.raises(KeyError):
             portfolio_profit = portfolio.profit("2025-01-01", "2020-12-31")
 
-
     def test_portfolio_profit_method_fails_with_non_existent_ticker(self):
         portfolio = Portfolio({
-            "AAPL": 100, 
+            "AAPL": 100,
             "XOM": 50,
             "GE": 2,
             "MSFT": 300
         })
         with pytest.raises(KeyError):
             portfolio_profit = portfolio.profit("2019-01-01", "2020-12-31")
+
+    def test_get_time_between_method_integer_dates(self):
+        portfolio = Portfolio({
+            "AAPL": 1,
+            "INNT": 1,
+            "GOOGL": 2,
+            "FB": 3
+        })
+        initial_date = "2019-01-01"
+        ending_date = "2020-12-31"
+
+        years_between_dates = portfolio.utils.get_time_between(
+            initial_date, ending_date, measure="year", format="%Y-%m-%d")
+
+        assert years_between_dates == 2
+
+        months_between_dates = portfolio.utils.get_time_between(
+            initial_date, ending_date, measure="month", format="%Y-%m-%d")
+
+        assert months_between_dates == 24
+
+        days_between_dates = portfolio.utils.get_time_between(
+            initial_date, ending_date, measure="day", format="%Y-%m-%d")
+
+        assert days_between_dates == 730
+
+    def test_get_time_between_method_fraction_dates(self):
+        portfolio = Portfolio({
+            "AAPL": 1,
+            "INNT": 1,
+            "GOOGL": 2,
+            "FB": 3
+        })
+        initial_date = "2019-01-01"
+        ending_date = "2020-12-22"
+
+        years_between_dates = portfolio.utils.get_time_between(
+            initial_date, ending_date, measure="year", format="%Y-%m-%d")
+
+        assert years_between_dates == 1.975
+
+        months_between_dates = portfolio.utils.get_time_between(
+            initial_date, ending_date, measure="month", format="%Y-%m-%d")
+
+        assert months_between_dates == 23.704
+
+        days_between_dates = portfolio.utils.get_time_between(
+            initial_date, ending_date, measure="day", format="%Y-%m-%d")
+
+        assert days_between_dates == 721
+
+    def test_portfolio_annualized_return_method_success(self):
+        portfolio = Portfolio({
+            "AAPL": 1,
+            "INNT": 1,
+            "GOOGL": 2,
+            "FB": 3
+        })
+        initial_date = "2019-01-01"
+        ending_date = "2020-12-31"
+
+        initial_investment = portfolio.sum_stock_profits(
+            portfolio.get_total_stocks_prices(initial_date))
+
+        profit = portfolio.profit(initial_date, ending_date)
+
+        # R
+        investment_return = profit/initial_investment
+
+        # t
+        years_between_dates = portfolio.utils.get_time_between(
+            initial_date, ending_date, measure="year", format="%Y-%m-%d")
+
+        # r
+        theoretical_annualized_return = math.pow(
+            (1 + investment_return), 1/years_between_dates) - 1
+
+        portfolio_annualized_return = portfolio.annualized_return(
+            initial_date, ending_date)
+
+
+        assert theoretical_annualized_return == portfolio_annualized_return
